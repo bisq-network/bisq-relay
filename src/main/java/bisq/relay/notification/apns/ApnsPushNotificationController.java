ea66269f9e86415c8a53d75ee868ce1da2735f86
Devin Bileck <603793+devinbileck@users.noreply.github.com>
2025-02-24 22:08:15 -0800

    Mask sensitive content in log

--- src/main/java/bisq/relay/notification/apns/ApnsPushNotificationController.java
+++ src/main/java/bisq/relay/notification/apns/ApnsPushNotificationController.java
@@ -34,12 +34,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.CompletableFuture;
 
+import static bisq.relay.util.MaskingUtil.maskSensitive;
+
 @RestController
 public class ApnsPushNotificationController extends PushNotificationController {
     private static final Logger LOG = LoggerFactory.getLogger(ApnsPushNotificationController.class);
 
     @Autowired
-    public ApnsPushNotificationController(final ApnsPushNotificationSender apnsSender, final ObjectMapper objectMapper) {
+    public ApnsPushNotificationController(
+            final ApnsPushNotificationSender apnsSender,
+            final ObjectMapper objectMapper) {
         super(apnsSender, objectMapper);
     }
 
@@ -50,7 +54,8 @@ public class ApnsPushNotificationController extends PushNotificationController {
             final HttpServletRequest httpRequest) {
 
         if (LOG.isInfoEnabled()) {
-            LOG.info("Handling APNs notification for device token [{}] from [{}]", deviceToken,
+            LOG.info("Handling APNs notification for device token [{}] from [{}]",
+                    maskSensitive(deviceToken),
                     httpRequest.getHeader(HttpHeaders.USER_AGENT));
         }
 
