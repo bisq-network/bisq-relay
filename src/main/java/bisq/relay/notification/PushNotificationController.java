543b198ae506e2a8fb6c8ea97fd03bc6183f9de4
Devin Bileck <603793+devinbileck@users.noreply.github.com>
2026-07-02 21:43:50 -0700

    Remove filter usage from PushNotificationController

--- src/main/java/bisq/relay/notification/PushNotificationController.java
+++ src/main/java/bisq/relay/notification/PushNotificationController.java
@@ -20,42 +20,34 @@ package bisq.relay.notification;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.Nonnull;
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
-import java.io.IOException;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
-public abstract class PushNotificationController implements Filter {
+public abstract class PushNotificationController {
     private static final Logger LOG = LoggerFactory.getLogger(PushNotificationController.class);
 
     private final PushNotificationSender pushNotificationSender;
     private final ObjectMapper objectMapper;
 
-    protected PushNotificationController(@Nonnull final PushNotificationSender pushNotificationSender,
-                                         @Nonnull final ObjectMapper objectMapper) {
+    protected PushNotificationController(
+            @Nonnull final PushNotificationSender pushNotificationSender,
+            @Nonnull final ObjectMapper objectMapper
+    ) {
         this.pushNotificationSender = Objects.requireNonNull(pushNotificationSender);
         this.objectMapper = Objects.requireNonNull(objectMapper);
-    }
-
-    @Override
-    public void doFilter(final ServletRequest request, final ServletResponse response,
-                         final FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-        httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        chain.doFilter(request, response);
     }
 
     // TODO implement rate limiting, can use resilience4j
     //  Ref: https://www.baeldung.com/spring-boot-resilience4j
     public CompletableFuture<ResponseEntity<String>> handleRequest(
             @Nonnull final String deviceToken,
-            @Nonnull final PushNotificationMessage pushNotificationMessage) {
+            @Nonnull final PushNotificationMessage pushNotificationMessage
+    ) {
         Objects.requireNonNull(deviceToken);
         Objects.requireNonNull(pushNotificationMessage);
 
@@ -66,13 +58,27 @@ public abstract class PushNotificationController implements Filter {
                         body = objectMapper.writeValueAsString(notificationResult);
                     } catch (JsonProcessingException e) {
                         LOG.error("Unable to serialize notification result; {}\n{}", e.getMessage(), notificationResult);
-                        return ResponseEntity.internalServerError().body("");
+                        return ResponseEntity
+                                .internalServerError()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body("");
                     }
+
                     if (notificationResult.wasAccepted()) {
-                        return ResponseEntity.ok().body(body);
+                        return ResponseEntity
+                                .ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body(body);
                     }
-                    return ResponseEntity.badRequest().body(body);
+
+                    return ResponseEntity
+                            .badRequest()
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .body(body);
                 })
-                .exceptionally(cause -> ResponseEntity.internalServerError().body(""));
+                .exceptionally(cause -> ResponseEntity
+                        .internalServerError()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(""));
     }
 }
